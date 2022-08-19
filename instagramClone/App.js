c9aca86e843c6ee10e3d75c01b54fcc46974d25c
kodluyoruz <info@kodluyoruz.org>
2022-08-19 18:34:07 +0300

    App.js düzenlendi.

--- instagramClone/App.js
+++ instagramClone/App.js
@@ -1,112 +1,93 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React from "react";
+import MaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
+import {NavigationContainer} from "@react-navigation/native";
+import {createBottomTabNavigator} from "@react-navigation/bottom-tabs";
+import {createNativeStackNavigator} from "@react-navigation/native-stack";
+import Home from './src/components/screens/Home';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
+const App = () => {
+
+    const Stack = createNativeStackNavigator();
+
+    const Tab = createBottomTabNavigator();
+
+    const BottomTabScreen = () => {
+        return(
+            <Tab.Navigator
+            // Daha sonrasında geliştirilebilmesi için tıkladıkça değişebilen bir  yapı mevcut.
+            screenOptions={({route}) => ({
+                tabBarShowLabel: false,
+                headerShown: false,
+                tabBarStyle:{
+                    height: 50
+                },
+                // Alt Menüdeki ikonların tıklanınca değişmesi.
+                tabBarIcon: ({focused, size, colour}) => {
+                    let iconName;
+                    ({ iconName, size, colour } = newFunction(route, focused, size, colour));
+
+
+                    return <MaterialCommunityIcons name = {iconName} size={size} color={colour}/>
+                }
+
+            })}>
+                {/* Alt Menüdeki sekme isimleri ve sayısı */}
+                <Tab.Screen name="Home" component={Home}/>
+                <Tab.Screen name="Search" component={Home}/>
+                <Tab.Screen name="Reels" component={Home}/>
+                <Tab.Screen name="Activity" component={Home}/>
+                <Tab.Screen name="Profile" component={Home}/>
+            </Tab.Navigator>
+        )
+    }
+
+
+    return (
+
+        <NavigationContainer>
+            <Stack.Navigator
+            screenOptions={{
+                headerShown: false
+            }}>
+                <Stack.Screen name="Bottom" component={BottomTabScreen}/>
+            </Stack.Navigator>
+        </NavigationContainer>
+    
+    );
 };
 
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
+//bu kısım daha sonra switch case ile yapılabilir.
+function newFunction(route, focused, size, colour) {
+    let iconName;
+    if (route.name === "Home") {
+        iconName = focused ? "home" : "home-outline";
+        size = focused ? size + 2 : size + 3;
+        colour = focused ? "black" : "#222224";
+    }
+    else if (route.name === "Search") {
+        iconName = "magnify";
+        size = focused ? size + 2 : size + 3;
+        colour = focused ? "black" : "#222224";
+    }
+    else if (route.name === "Reels") {
+        iconName = focused ? "play-circle" : "play-circle-outline";
+        size = focused ? size + 2 : size + 3;
+        colour = focused ? "black" : "#222224";
+    }
+    else if (route.name === "Activity") {
+        iconName = focused ? "shopping" : "shopping-outline";
+        size = focused ? size + 2 : size + 3;
+        colour = focused ? "black" : "#222224";
+    }
+    else if (route.name === "Profile") {
+        iconName = focused ? "account-circle" : "account-circle-outline";
+        size = focused ? size + 2 : size + 3;
+        colour = focused ? "black" : "#222224";
+    }
+    return { iconName, size, colour };
+}
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-};
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
 
 export default App;
